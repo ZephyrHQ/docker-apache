fe95ff96c16785a0d348e910eac7aa30fcc7368f
Nicolas de Marqué Fromentin <nicolas.demarque@gmail.com>
2017-11-26 14:35:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ CMD=$*
 if [[ "$ENV" = "dev" ]]
 then
     echo Dev environment activation
-    sed -e s/app.php/app_dev.php/g /etc/apache2/sites-available/vhost.conf > /tmp/vhost.conf
+    sed -e s/app.php/app_dev.php/g /vhost.conf > /tmp/vhost.conf
     sed -i -e "s/app\\\.php/app_dev\\\.php/g" /tmp/vhost.conf 
     sed -i -e "s/#ServerName/ServerName dev.zephyr-web.eu/g" /tmp/vhost.conf
     cat /tmp/vhost.conf > /etc/apache2/sites-available/vhost.conf
