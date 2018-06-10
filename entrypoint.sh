eca2f45ebc5dd869f5b0c9ec9b384c4996eb5c5f
Nicolas de Marqué Fromentin <nicolas.demarque@gmail.com>
2018-06-10 19:36:17 +0200

    Merge remote-tracking branch 'refs/remotes/origin/alpine'
    Conflicts:
    	Dockerfile
    	README.md
    	entrypoint.sh
    	httpd.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 [ -f "/var/run/apache2/apache2.pid" ] && rm /var/run/apache2/apache2.pid
 
 echo "Déplacement des fichiers de configuration"
+[ -f /conf/httpd.conf ] && cp /conf/httpd.conf /usr/local/apache2/conf/httpd.conf
 [ -f /conf/vhost.conf ] && cp /conf/vhost.conf /usr/local/apache2/conf/extra/httpd-vhosts.conf
 [ -f /conf/cert.pem ] && cp /conf/cert.pem /etc/ssl/cert.pem
 [ -f /conf/key.pem ] && cp /conf/key.pem /etc/ssl/key.pem
