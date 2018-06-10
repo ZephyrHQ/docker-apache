625bee50f82a31adf87fcf056a4d1343fae5be8e
Nicolas de Marqué Fromentin <nicolas.demarque@gmail.com>
2018-06-10 22:32:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 [ -f "/var/run/apache2/apache2.pid" ] && rm /var/run/apache2/apache2.pid
 
 echo "Déplacement des fichiers de configuration"
-[ -f /conf/httpd.conf ] && cp /conf/httpd.conf /usr/local/apache2/conf/httpd.conf
-[ -f /conf/vhost.conf ] && cp /conf/vhost.conf /usr/local/apache2/conf/extra/httpd-vhosts.conf
-[ -f /conf/cert.pem ] && cp /conf/cert.pem /etc/ssl/cert.pem
-[ -f /conf/key.pem ] && cp /conf/key.pem /etc/ssl/key.pem
+[ -f /conf/httpd.conf ] && echo copy httpd.conf && cp /conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+[ -f /conf/vhost.conf ] && echo copy vhost.conf && cp /conf/vhost.conf /usr/local/apache2/conf/extra/httpd-vhosts.conf
+[ -f /conf/cert.pem ] && echo copy cert && cp /conf/cert.pem /etc/ssl/cert.pem
+[ -f /conf/key.pem ] && echo copy key && cp /conf/key.pem /etc/ssl/key.pem
 
 cd ~
 
